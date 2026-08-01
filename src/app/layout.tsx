0069139f119609b4eeeeb13e93ba4465e18c884d
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-08-01 04:31:08 +0000

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable)}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col dark">
         <Navbar />
         {children}
         <Footer />
